f3ae46ff19c32d63f872228f0e493affa4f50de6
Mikhail Fedosov <tbs.micle@gmail.com>
2022-06-25 00:23:15 +0300

    feat: hover wireframe effect

--- app/components/Rock.jsx
+++ app/components/Rock.jsx
@@ -1,8 +1,11 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 export function Rock({ geometry }) {
+  const DEFAULT_COLOR = "#2f343c";
+  const [wireframe, setWireframe] = useState(false);
+  const [color, setColor] = useState(DEFAULT_COLOR);
   const rock = useRef();
   useFrame(({ clock }) => {
     rock.current.rotation.y = clock.getElapsedTime() / 3.0;
@@ -14,8 +17,19 @@ export function Rock({ geometry }) {
       <ambientLight intensity={0.8} color={[1, 1, 1]} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <mesh ref={rock} geometry={geometry}>
-        <meshStandardMaterial color="#2f343c" roughness={0.1} wireframe={false} side={THREE.DoubleSide} />
+      <mesh
+        ref={rock}
+        geometry={geometry}
+        onPointerEnter={() => {
+          setWireframe(true);
+          setColor("#fff");
+        }}
+        onPointerLeave={() => {
+          setWireframe(false);
+          setColor(DEFAULT_COLOR);
+        }}
+      >
+        <meshStandardMaterial color={color} roughness={0.1} wireframe={wireframe} side={THREE.DoubleSide} />
       </mesh>
     </>
   );
