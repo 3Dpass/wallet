63e59ac06a196d712fe02bb8b278049ccaa2044d
Mikhail Fedosov <tbs.micle@gmail.com>
2022-07-21 20:58:40 +0300

    feat: hide paste button if it is not supported

--- app/components/dialogs/DialogImportAddress.tsx
+++ app/components/dialogs/DialogImportAddress.tsx
@@ -1,8 +1,13 @@
 import { Button, Classes, Dialog, Intent, TextArea } from "@blueprintjs/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DialogImportAddress({ isOpen, onImport, onClose }) {
   const [value, setValue] = useState("");
+  const [canPaste, setCanPaste] = useState(false);
+
+  useEffect(() => {
+    setCanPaste(navigator.clipboard.readText !== undefined);
+  }, []);
 
   function handleOnOpening() {
     setValue("");
@@ -22,9 +27,11 @@ export default function DialogImportAddress({ isOpen, onImport, onClose }) {
       <Dialog isOpen={isOpen} usePortal={true} onOpening={handleOnOpening} onClose={onClose} className="w-[90%] sm:w-[640px]">
         <div className={Classes.DIALOG_BODY}>
           <TextArea className="w-full font-mono" rows={5} onChange={handleSeedPhraseChange} value={value} />
-          <div className="text-center mt-4">
-            <Button icon="clipboard" text="Paste" onClick={handlePaste} />
-          </div>
+          {canPaste && (
+            <div className="text-center mt-4">
+              <Button icon="clipboard" text="Paste" onClick={handlePaste} />
+            </div>
+          )}
         </div>
         <div className={Classes.DIALOG_FOOTER}>
           <div className={Classes.DIALOG_FOOTER_ACTIONS}>
