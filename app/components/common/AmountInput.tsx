7c1cdee562da35c6815bc253b7ea2f6a002a3303
WlinkNET <52631720+WlinkNET@users.noreply.github.com>
2023-03-16 13:27:32 +0100

    tip feature added (#9)
    * tip feature added
    
    * Fix placeholder and code duplicity
    
    * cleanup unused code
    
    * prettier check
    
    * final check

--- app/components/common/AmountInput.tsx
+++ app/components/common/AmountInput.tsx
@@ -6,9 +6,10 @@ import { NumericInput, Tag } from "@blueprintjs/core";
 type IProps = {
   disabled: boolean;
   onValueChange: (valueAsNumber: number, valueAsString: string) => void;
+  placeholder?: string;
 };
 
-export default function AmountInput({ disabled, onValueChange }: IProps) {
+export default function AmountInput({ disabled, onValueChange, placeholder }: IProps) {
   const api = useAtomValue(apiAtom);
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [amount, setAmount] = useState("");
@@ -30,7 +31,7 @@ export default function AmountInput({ disabled, onValueChange }: IProps) {
       className="mb-2"
       large={true}
       leftIcon="send-to"
-      placeholder="Amount"
+      placeholder={placeholder || "Amount"}
       onValueChange={handleAmountChange}
       value={amount}
       fill={true}
--- app/components/dialogs/DialogSendFunds.tsx
+++ app/components/dialogs/DialogSendFunds.tsx
@@ -24,6 +24,8 @@ export default function DialogSendFunds({ pair, isOpen, onClose, onAfterSubmit }
     address: "",
     amount: "",
     amount_number: 0,
+    tips: "",
+    tips_number: 0,
   };
   const [data, setData] = useState(dataInitial);
 
@@ -34,6 +36,10 @@ export default function DialogSendFunds({ pair, isOpen, onClose, onAfterSubmit }
 
   function handleAmountChange(valueAsNumber, valueAsString) {
     setData((prev) => ({ ...prev, amount: valueAsString, amount_number: valueAsNumber }));
+  }
+
+  function handleTipsChange(valueAsNumber, valueAsString) {
+    setData((prev) => ({ ...prev, tips: valueAsString, tips_number: valueAsNumber }));
   }
 
   useEffect(() => {
@@ -47,7 +53,10 @@ export default function DialogSendFunds({ pair, isOpen, onClose, onAfterSubmit }
     setIsLoading(true);
     try {
       const value = BigInt(data.amount_number * 1_000_000_000_000);
-      await api.tx.balances.transfer(data.address, value).signAndSend(pair);
+      const tips = BigInt(data.tips_number * 1_000_000_000_000);
+      const tx = api.tx.balances.transfer(data.address, value);
+      const options = tips > 0 ? { tip: tips.toString() } : undefined;
+      await tx.signAndSend(pair, options);
       toaster &&
         toaster.show({
           icon: "endorsed",
@@ -83,6 +92,7 @@ export default function DialogSendFunds({ pair, isOpen, onClose, onAfterSubmit }
           leftElement={addressIcon}
         />
         <AmountInput disabled={isLoading} onValueChange={handleAmountChange} />
+        <AmountInput disabled={isLoading} onValueChange={handleTipsChange} placeholder="Enter optional tips to increase transaction priority" />
       </div>
       <div className={Classes.DIALOG_FOOTER}>
         <div className={Classes.DIALOG_FOOTER_ACTIONS}>
