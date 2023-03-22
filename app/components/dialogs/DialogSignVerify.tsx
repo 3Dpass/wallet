8ce7c88622628109e9affb2e73a5aedb0b916dec
WlinkNET <52631720+WlinkNET@users.noreply.github.com>
2023-03-22 23:32:57 +0000

    Pending changes exported from your codespace

--- app/components/dialogs/DialogSignVerify.tsx
+++ app/components/dialogs/DialogSignVerify.tsx
@@ -1,4 +1,4 @@
-import { Button, Classes, Dialog, Icon, InputGroup, Intent } from "@blueprintjs/core";
+import { Button, Classes, Dialog, Icon, InputGroup, Intent, RadioGroup, Radio, TextArea } from "@blueprintjs/core";
 import { cryptoWaitReady, blake2AsHex, signatureVerify, decodeAddress } from "@polkadot/util-crypto";
 import type { KeyringPair } from "@polkadot/keyring/types";
 import { web3FromSource } from "@polkadot/extension-dapp";
@@ -6,7 +6,7 @@ import { u8aToHex, hexToU8a } from "@polkadot/util";
 import { keyring } from "@polkadot/ui-keyring";
 import { toasterAtom } from "../../atoms";
 import { useAtomValue } from "jotai";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 type IProps = {
   pair: KeyringPair;
@@ -21,13 +21,15 @@ export default function DialogSignAndVerify({ pair, isOpen, onClose }: IProps) {
   const [publicKey, setPublicKey] = useState("");
   const [address, setAddress] = useState("");
   const [formattedMessage, setFormattedMessage] = useState("");
-  const [showFormattedMessage, setShowFormattedMessage] = useState(false);
+  const [messageType, setMessageType] = useState("sign");
+  const [pastedData, setPastedData] = useState("");
 
   function handleOnOpening() {
     setMessage("");
     setSignedMessage("");
     setPublicKey("");
     setAddress(pair.address);
+    setMessageType("sign");
   }
 
   async function handleSignClick() {
@@ -78,29 +80,57 @@ export default function DialogSignAndVerify({ pair, isOpen, onClose }: IProps) {
 
   async function handleVerifyClick() {
     try {
-      const signatureU8a = hexToU8a(signedMessage);
-      const publicKeyU8a = hexToU8a(publicKey);
-      const messageHash = blake2AsHex(message, 256);
+      if (messageType === "verify") {
+        // Extract message and signedMessage from pastedData
+        const messageRegex = /--\s*Start message\s*--\n?([\s\S]*?)\n?--\s*End message\s*--/g;
+const signatureRegex = /--\s*Start P3D wallet signature\s*--\n?([\s\S]*?)\n?--\s*End P3D wallet signature\s*--/g;
+const publicKeyRegex = /--\s*Start public key\s*--\n?([\s\S]*?)\n?--\s*End public key\s*--/g;
 
-      const isSigned = await signatureVerify(messageHash, signatureU8a, publicKeyU8a);
-      if (isSigned.isValid) {
-        toaster &&
-          toaster.show({
-            icon: "endorsed",
-            intent: Intent.SUCCESS,
-            message: "Signature is valid",
-          });
-        setFormattedMessage(
-          `-- Start message --\n${address} ${message}\n-- End message --\n\n-- Start P3D wallet signature --\n${signedMessage}\n-- End P3D wallet signature --`
-        );
-        setShowFormattedMessage(true);
-      } else {
-        toaster &&
-          toaster.show({
-            icon: "error",
-            intent: Intent.DANGER,
-            message: "Signature is invalid",
-          });
+        console.log("Message matchR:", messageRegex);
+console.log("Signature matchR:", signatureRegex);
+console.log("Public key matchR:", publicKeyRegex);
+  
+        const messageMatch = messageRegex.exec(pastedData);
+        const signatureMatch = signatureRegex.exec(pastedData);
+        const publicKeyMatch = publicKeyRegex.exec(pastedData);
+        console.log("Message match:", messageMatch);
+console.log("Signature match:", signatureMatch);
+console.log("Public key match:", publicKeyMatch);
+        
+        if (messageMatch && signatureMatch && publicKeyMatch) {
+          const message = messageMatch[1].trim();
+          const signedMessage = signatureMatch[1].trim();
+          const publicKey = publicKeyMatch[1].trim();
+          console.log("message: ", message);
+          console.log("signedMessage: ", signedMessage);
+          console.log("publicKey: ", publicKey );
+  
+          // Convert hex signatures to U8a
+          const signatureU8a = hexToU8a(signedMessage);
+          const messageHash = blake2AsHex(message, 256);
+          const publicKeyU8a = hexToU8a(publicKey);
+  
+          // Call the verification function with extracted data
+          const isSigned = await signatureVerify(messageHash, signatureU8a, publicKeyU8a);
+  
+          if (isSigned.isValid) {
+            toaster &&
+              toaster.show({
+                icon: "endorsed",
+                intent: Intent.SUCCESS,
+                message: "Signature is valid",
+              });
+          } else {
+            toaster &&
+              toaster.show({
+                icon: "error",
+                intent: Intent.DANGER,
+                message: "Signature is invalid",
+              });
+          }
+        } else {
+          console.error("Unable to extract message and signature from pasted data");
+        }
       }
     } catch (e) {
       toaster &&
@@ -111,22 +141,43 @@ export default function DialogSignAndVerify({ pair, isOpen, onClose }: IProps) {
         });
     }
   }
+  const handleCopyClick = () => {
+    const messageToCopy =
+  setFormattedMessage &&
+  `-- Start message --\n ${message}\n-- End message --\n\n-- Start P3D wallet signature --\n${signedMessage}\n-- End P3D wallet signature --\n\n-- Start public key --\n${publicKey}\n-- End public key --`;
 
-  function copyFormattedMessage() {
-    navigator.clipboard.writeText(formattedMessage).then(() => {
-      toaster &&
-        toaster.show({
-          icon: "clipboard",
-          intent: Intent.SUCCESS,
-          message: "Formatted message copied",
-        });
+
+    navigator.clipboard.writeText(messageToCopy).then(() => {
+      toaster.show({
+        message: "Message copied to clipboard!",
+        intent: Intent.SUCCESS,
+        icon: "tick",
+      });
     });
-  }
+  };
+  const textareaRef = useRef();
+  const handlePasteClick = async () => {
+    if (messageType === "verify") {
+      try {
+        const text = await navigator.clipboard.readText();
+        setPastedData(text);
+        console.log("Pasted text:", text);
+      } catch (err) {
+        console.error("Failed to paste content: ", err);
+      }
+    }
+  };
 
   return (
     <>
       <Dialog isOpen={isOpen} onClose={onClose} onOpening={handleOnOpening} title="Sign and Verify Message" style={{ width: "550px" }}>
         <div className={Classes.DIALOG_BODY}>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <RadioGroup inline onChange={(e) => setMessageType(e.currentTarget.value)} selectedValue={messageType}>
+              <Radio label="Sign" value="sign" />
+              <Radio label="Verify" value="verify" />
+            </RadioGroup>
+          </div>
           <h6>Address</h6>
           <InputGroup
             large={true}
@@ -136,54 +187,84 @@ export default function DialogSignAndVerify({ pair, isOpen, onClose }: IProps) {
             onChange={(e) => setAddress(e.target.value)}
             value={address}
             leftElement={<Icon icon="credit-card" />}
-            disabled={true}
+            disabled={false}
           />
           <h6>Message</h6>
-          <InputGroup
-            large={true}
-            className="mb-2"
-            spellCheck={false}
-            placeholder="Message to sign"
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-            leftElement={<Icon icon="edit" />}
-          />
-          <h6>Signature key</h6>
-          <InputGroup
-            large={true}
-            className="mb-2"
-            spellCheck={false}
-            placeholder="Signature"
-            onChange={(e) => setSignedMessage(e.target.value)}
-            value={signedMessage}
-            leftElement={<Icon icon="key" />}
-          />
-        </div>
-        <div className={Classes.DIALOG_FOOTER}>
-          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-            <Button onClick={onClose} text="Cancel" />
-            <Button intent={Intent.PRIMARY} onClick={handleSignClick} text="Sign" />
-            <Button intent={Intent.PRIMARY} onClick={handleVerifyClick} text="Verify" />
-          </div>
-        </div>
-      </Dialog>
-      <Dialog isOpen={showFormattedMessage} onClose={() => setShowFormattedMessage(false)} title="Formatted Message" style={{ width: "550px" }}>
-        <div className={Classes.DIALOG_BODY}>
-          <pre
-            style={{
-              whiteSpace: "pre-wrap",
-              wordWrap: "break-word",
-              textAlign: "center",
-            }}
-          >
-            {formattedMessage}
-          </pre>
-        </div>
-        <div className={Classes.DIALOG_FOOTER}>
-          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-            <Button onClick={() => setShowFormattedMessage(false)} text="Close" />
-            <Button intent={Intent.PRIMARY} onClick={copyFormattedMessage} text="Copy" />
-          </div>
+          {messageType === "sign" ? (
+            <>
+              <InputGroup
+                large={true}
+                className="mb-2"
+                spellCheck={false}
+                placeholder="Message"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+                leftElement={<Icon icon="edit" />}
+                disabled={false}
+              />
+              <h6>Signed message</h6>
+              <div style={{ position: "relative" }}>
+                <TextArea
+                  className="bp4-code-block bp4-docs-code-block blueprint-dark"
+                  style={{
+                    height: "220px",
+                    width: "100%",
+                    padding: "10px",
+                    textAlign: "left",
+                  }}
+                  readOnly
+                  value={
+                    setFormattedMessage &&
+                    `-- Start message --\n ${message}\n-- End message --\n\n-- Start P3D wallet signature --\n${signedMessage}\n-- End P3D wallet signature --\n\n-- Start public key --\n${publicKey}\n-- End public key --`
+                  }
+                />
+                <Button
+                  className="bp3-dark"
+                  onClick={handleCopyClick}
+                  text="Copy"
+                  style={{
+                    position: "absolute",
+                    bottom: "10px",
+                    right: "10px",
+                  }}
+                />
+              </div>
+              <div className={Classes.DIALOG_FOOTER}>
+                <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                  <Button onClick={onClose} text="Cancel" />
+                  <Button intent={Intent.PRIMARY} onClick={handleSignClick} text="Sign" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <TextArea
+                className="bp4-code-block bp4-docs-code-block blueprint-dark"
+                style={{
+                  height: "220px",
+                  width: "100%",
+                  padding: "10px",
+                  textAlign: "left",
+                }}
+                value={pastedData}
+                onChange={(e) => setPastedData(e.target.value)}
+              />
+              <Button
+                intent={Intent.PRIMARY}
+                onClick={handlePasteClick}
+                text="Paste"
+                style={{
+                  marginTop: "10px",
+                }}
+              />
+              <div className={Classes.DIALOG_FOOTER}>
+                <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                  <Button onClick={onClose} text="Cancel" />
+                  <Button intent={Intent.PRIMARY} onClick={handleVerifyClick} text="Verify" />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </Dialog>
     </>
