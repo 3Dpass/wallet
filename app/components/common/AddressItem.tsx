5178f5e4f3f1658e94fdf659c53907f6f4addfdb
evgenii <evgenii@marketmuse.com>
2023-04-22 22:56:15 +0400

    Identity UI: first steps

--- app/components/common/AddressItem.tsx
+++ app/components/common/AddressItem.tsx
@@ -1,11 +1,18 @@
+import { Icon, IconSize } from "@blueprintjs/core";
 import { Link } from "@remix-run/react";
 import { AddressIcon } from "./AddressIcon";
 
 export function AddressItem({ address }: { address: string }) {
   return (
-    <Link to={`/address/${address}`} className="flex text-white no-underline group">
+    <Link to={`/address/${address}`} className="flex text-white no-underline group" onMouseEnter={() => console.log('hit')}>
       <AddressIcon address={address} />
       <div className="font-mono ml-2 text-ellipsis overflow-hidden border-b border-b-gray-500 group-hover:border-b-white">{address}</div>
+      <Icon
+        className="bp4-icon bp4-intent-success"
+        style={{'position': 'absolute', 'right': '5px'}}
+        icon="endorsed"
+        size={IconSize.LARGE}
+      />
     </Link>
   );
 }
