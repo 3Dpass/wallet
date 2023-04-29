b733cef133ed0d5e842fb613f9f0108ac0a79c8c
evgenii <evgenii@marketmuse.com>
2023-04-30 02:09:22 +0400

    Mining Pool: add remove miner and add miner buttons

---
+++ app/components/dialogs/DialogRemoveMiner.tsx
@@ -0,0 +1,138 @@
+import { Button, Classes, Dialog, Intent, MenuItem } from "@blueprintjs/core";
+import { ItemPredicate, ItemRenderer, Select2 } from "@blueprintjs/select";
+import { MouseEventHandler, useEffect, useState } from "react";
+import type { KeyringPair } from "@polkadot/keyring/types";
+
+import type { SignerOptions } from "@polkadot/api/types";
+import { signAndSend } from "../../utils/sign";
+import useApi from "../../hooks/useApi";
+import useToaster from "../../hooks/useToaster";
+
+type IProps = {
+  pair: KeyringPair;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function DialogRemoveMiner({ isOpen, onClose, pair }: IProps) {
+  const api = useApi();
+  const toaster = useToaster();
+  const [canSubmit, setCanSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<string[]>([]);
+  const [memberToRemove, setMemberToRemove] = useState<string>("");
+
+  async function loadMembers() {
+    if (!api) {
+      return;
+    }
+    const members = await api.query.miningPool.pools(pair.address);
+    setData(members.toHuman() as string[]);
+  }
+
+  function handleOnOpening() {
+    setIsLoading(false);
+    void loadMembers();
+  }
+
+  useEffect(() => {
+    setCanSubmit(api !== undefined);
+  }, [api, {}]);
+
+  async function handleSubmitClick() {
+    if (!api) {
+      return;
+    }
+    if (memberToRemove == "") {
+      return;
+    }
+    const isLocked = pair.isLocked && !pair.meta.isInjected;
+    if (isLocked) {
+      toaster.show({
+        icon: "error",
+        intent: Intent.DANGER,
+        message: "Account is locked",
+      });
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const tx = api.tx.miningPool.removeMember(memberToRemove);
+      const options: Partial<SignerOptions> = {};
+      await signAndSend(tx, pair, options);
+      toaster.show({
+        icon: "endorsed",
+        intent: Intent.SUCCESS,
+        message: "The member was removed from the mining pool",
+      });
+    } catch (e: any) {
+      toaster.show({
+        icon: "error",
+        intent: Intent.DANGER,
+        message: e.message,
+      });
+    } finally {
+      setIsLoading(false);
+      onClose();
+    }
+  }
+
+  const filterMember: ItemPredicate<string> = (query, poolId, _index, exactMatch) => {
+    const normalizedId = poolId.toLowerCase();
+    const normalizedQuery = query.toLowerCase();
+    if (exactMatch) {
+      return normalizedId === normalizedQuery;
+    } else {
+      return normalizedId.indexOf(normalizedQuery) >= 0;
+    }
+  };
+
+  const renderMemberId: ItemRenderer<string> = (poolId, { handleClick, handleFocus, modifiers, query }) => {
+    if (!modifiers.matchesPredicate) {
+      return null;
+    }
+    return (
+      <MenuItem
+        active={modifiers.active}
+        disabled={modifiers.disabled}
+        key={poolId}
+        label={poolId}
+        onClick={handleClick as MouseEventHandler}
+        onFocus={handleFocus}
+        roleStructure="listoption"
+        text=""
+      />
+    );
+  };
+
+  return (
+    <Dialog isOpen={isOpen} usePortal={true} onOpening={handleOnOpening} title="Remove a miner" onClose={onClose} className="w-[90%] sm:w-[640px]">
+      <div className={`${Classes.DIALOG_BODY} flex flex-col gap-3`}>
+        <Select2
+          items={data}
+          itemPredicate={filterMember}
+          itemRenderer={renderMemberId}
+          noResults={<MenuItem disabled={true} text="No results." roleStructure="listoption" />}
+          onItemSelect={(m) => setMemberToRemove(m)}
+          popoverProps={{ matchTargetWidth: true }}
+          fill={true}
+        >
+          <Button text={data} rightIcon="double-caret-vertical" placeholder="Select a miner" className={`${Classes.CONTEXT_MENU} ${Classes.FILL}`} />
+        </Select2>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={onClose} text="Cancel" disabled={isLoading} />
+          <Button
+            intent={Intent.PRIMARY}
+            disabled={isLoading || !canSubmit}
+            onClick={handleSubmitClick}
+            icon="remove"
+            loading={isLoading}
+            text="Remove a miner"
+          />
+        </div>
+      </div>
+    </Dialog>
+  );
+}
