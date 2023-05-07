94eee93f4315f9aeaa0b83a4068dc8e069fccca1
Mikhail Fedosov <tbs.micle@gmail.com>
2023-05-08 00:45:57 +0300

    refactor: Remove unused useSS58Format hook

--- app/hooks/useSS58Format.tsx
+++
@@ -1,7 +0,0 @@
-import { useAtomValue } from "jotai";
-import { formatOptionsAtom } from "../atoms";
-
-export function useSS58Format(): number | false {
-  const formatOptions = useAtomValue(formatOptionsAtom);
-  return formatOptions ? formatOptions.chainSS58 : false;
-}
