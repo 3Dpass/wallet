9b256e6d0c48f47eb71ff5f6f3dabeafb0620256
Mikhail Fedosov <tbs.micle@gmail.com>
2025-01-24 01:11:15 +0400

    feat(block): refactor Block component to use ThreeDObject for rendering

--- app/components/block/Block.client.tsx
+++ app/components/block/Block.client.tsx
@@ -1,87 +1,107 @@
-import { Suspense } from "react";
+import { Buffer } from "node:buffer";
+import {
+	Button,
+	Card,
+	Classes,
+	Elevation,
+	Icon,
+	Popover,
+} from "@blueprintjs/core";
+import { Canvas } from "@react-three/fiber";
 import { Link } from "@remix-run/react";
-import { Button, Card, Classes, Elevation, Icon, Popover } from "@blueprintjs/core";
-import Object from "./Object.client";
+import { useAtomValue } from "jotai";
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+import { bestNumberFinalizedAtom } from "../../atoms";
 import TitledValue from "../common/TitledValue";
 import type { IBlock } from "../types";
-import { Buffer } from "buffer";
-import { useAtomValue } from "jotai";
-import { bestNumberFinalizedAtom } from "../../atoms";
-import { Canvas } from "@react-three/fiber";
-import { useTranslation } from "react-i18next";
+import ThreeDObject from "./ThreeDObject.client";
 
 interface IProps {
-  block: IBlock;
+	block: IBlock;
 }
 
 export default function Block({ block }: IProps) {
-  const { t } = useTranslation();
-  const bestNumberFinalized = useAtomValue(bestNumberFinalizedAtom);
-  const objectHashAlgo = Buffer.from(block.objectHashAlgo, "hex").toString("utf8");
-  const blockNumber = block.block.header.number.toNumber();
-  const objectBase64 = Buffer.from(block.objectObj).toString("base64");
-  const downloadUrl = `data:text/plain;base64,${objectBase64}`;
-  const downloadFilename = `3dpass-${block.block.header.number.toString()}.obj`;
+	const { t } = useTranslation();
+	const bestNumberFinalized = useAtomValue(bestNumberFinalizedAtom);
+	const objectHashAlgo = Buffer.from(block.objectHashAlgo, "hex").toString(
+		"utf8",
+	);
+	const blockNumber = block.block.header.number.toNumber();
+	const objectBase64 = Buffer.from(block.objectObj).toString("base64");
+	const downloadUrl = `data:text/plain;base64,${objectBase64}`;
+	const downloadFilename = `3dpass-${block.block.header.number.toString()}.obj`;
 
-  return (
-    <Card elevation={Elevation.ZERO}>
-      <div className="flex justify-between items-start">
-        <TitledValue
-          title={t("root.lbl_block")}
-          value={
-            blockNumber <= bestNumberFinalized ? (
-              <>
-                <Link to={`/block/${blockNumber}`} target="_blank" rel="noopener noreferrer">
-                  {blockNumber.toLocaleString()}
-                </Link>{" "}
-                <div className="small-title">✓ {t("root.lbl_finalized")}</div>
-              </>
-            ) : (
-              <>
-                {blockNumber.toLocaleString()}
-                <div className="small-title">{t("root.lbl_block_not_finalized")}</div>
-              </>
-            )
-          }
-        />
-        <div className="flex gap-2">
-          <a className={Classes.BUTTON} href={downloadUrl} download={downloadFilename}>
-            <Icon icon="download" />
-          </a>
-          <Popover
-            content={
-              <div className="p-4">
-                {block.objectHashAlgo && (
-                  <div className="font-mono mb-2">
-                    {t("root.lbl_object_hash_algo")} <strong>{objectHashAlgo}</strong>
-                  </div>
-                )}
-                <code className="block text-xs">
-                  {block.objectHashes.map((hash, index) => {
-                    return <div key={index}>{hash}</div>;
-                  })}
-                </code>
-              </div>
-            }
-          >
-            <Button icon="info-sign" />
-          </Popover>
-        </div>
-      </div>
-      <div className="w-full h-[300px]">
-        <Canvas
-          camera={{
-            fov: 30,
-            near: 0.1,
-            far: 1000,
-            position: [0, 0, 2],
-          }}
-        >
-          <Suspense fallback={null}>
-            <Object geometry={block.object3d.geometry} />
-          </Suspense>
-        </Canvas>
-      </div>
-    </Card>
-  );
+	return (
+		<Card elevation={Elevation.ZERO}>
+			<div className="flex justify-between items-start">
+				<TitledValue
+					title={t("root.lbl_block")}
+					value={
+						blockNumber <= bestNumberFinalized ? (
+							<>
+								<Link
+									to={`/block/${blockNumber}`}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{blockNumber.toLocaleString()}
+								</Link>{" "}
+								<div className="small-title">✓ {t("root.lbl_finalized")}</div>
+							</>
+						) : (
+							<>
+								{blockNumber.toLocaleString()}
+								<div className="small-title">
+									{t("root.lbl_block_not_finalized")}
+								</div>
+							</>
+						)
+					}
+				/>
+				<div className="flex gap-2">
+					<a
+						className={Classes.BUTTON}
+						href={downloadUrl}
+						download={downloadFilename}
+					>
+						<Icon icon="download" />
+					</a>
+					<Popover
+						content={
+							<div className="p-4">
+								{block.objectHashAlgo && (
+									<div className="font-mono mb-2">
+										{t("root.lbl_object_hash_algo")}{" "}
+										<strong>{objectHashAlgo}</strong>
+									</div>
+								)}
+								<code className="block text-xs">
+									{block.objectHashes.map((hash) => (
+										<div key={hash}>{hash}</div>
+									))}
+								</code>
+							</div>
+						}
+					>
+						<Button icon="info-sign" />
+					</Popover>
+				</div>
+			</div>
+			<div className="w-full h-[300px]">
+				<Canvas
+					camera={{
+						fov: 30,
+						near: 0.1,
+						far: 1000,
+						position: [0, 0, 2],
+					}}
+				>
+					<Suspense fallback={null}>
+						<ThreeDObject geometry={block.object3d.geometry} />
+					</Suspense>
+				</Canvas>
+			</div>
+		</Card>
+	);
 }
---
+++ app/components/block/ThreeDObject.client.tsx
@@ -0,0 +1,65 @@
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import type { BufferGeometry, Mesh } from "three";
+import * as THREE from "three";
+
+const textureUrl = "/textures/space.jpg";
+const textureUrls = [
+	textureUrl,
+	textureUrl,
+	textureUrl,
+	textureUrl,
+	textureUrl,
+	textureUrl,
+];
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const textureCube = cubeTextureLoader.load(textureUrls);
+textureCube.wrapS = THREE.MirroredRepeatWrapping;
+textureCube.wrapT = THREE.MirroredRepeatWrapping;
+textureCube.mapping = THREE.CubeRefractionMapping;
+
+interface IProps {
+	geometry: BufferGeometry;
+}
+
+export default function ThreeDObject({ geometry }: IProps) {
+	const meshRef = useRef<Mesh>(null);
+	const [scaled, setScaled] = useState(false);
+
+	useEffect(() => {
+		// scale object to fit in a viewport
+		if (!scaled && meshRef.current) {
+			const bbox = new THREE.Box3().setFromObject(meshRef.current);
+			const size = bbox.getSize(new THREE.Vector3());
+			const maxDim = Math.max(size.x, size.y, size.z);
+			const scale = 1.0 / maxDim;
+			meshRef.current.scale.set(scale, scale, scale);
+			setScaled(true);
+		}
+	}, [scaled]);
+
+	useFrame(({ clock }) => {
+		if (!meshRef.current) {
+			return;
+		}
+		meshRef.current.rotation.set(
+			clock.getElapsedTime() / 10.0,
+			clock.getElapsedTime() / 10.0,
+			clock.getElapsedTime() / 10.0,
+		);
+	});
+
+	return (
+		<>
+			<mesh ref={meshRef} geometry={geometry}>
+				<meshPhongMaterial
+					emissive="#fff"
+					envMap={textureCube}
+					refractionRatio={0.7}
+					reflectivity={1}
+					flatShading
+				/>
+			</mesh>
+		</>
+	);
+}
