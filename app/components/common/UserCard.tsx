d495b2a7d95bdf9d42745a8a47995306c28336c8
Mikhail Fedosov <tbs.micle@gmail.com>
2025-01-24 01:01:49 +0400

    chore: lint

--- app/components/common/UserCard.tsx
+++ app/components/common/UserCard.tsx
@@ -1,101 +1,130 @@
-import { Card, Classes, HTMLTable, Icon, IconSize, Tag } from "@blueprintjs/core";
-import { AccountName } from "./AccountName";
+import {
+	Card,
+	Classes,
+	HTMLTable,
+	Icon,
+	IconSize,
+	Tag,
+} from "@blueprintjs/core";
 import { useTranslation } from "react-i18next";
+import { AccountName } from "./AccountName";
 
 type IRegistrarInfoItem = {
-  Raw: string;
+	Raw: string;
 };
 
 type IRegistrarInfo = {
-  display: IRegistrarInfoItem;
-  email: IRegistrarInfoItem;
-  web: IRegistrarInfoItem;
-  twitter: IRegistrarInfoItem;
-  image: IRegistrarInfoItem;
-  additional: IRegistrarInfoItem[][];
+	display: IRegistrarInfoItem;
+	email: IRegistrarInfoItem;
+	web: IRegistrarInfoItem;
+	twitter: IRegistrarInfoItem;
+	image: IRegistrarInfoItem;
+	additional: IRegistrarInfoItem[][];
 };
 
 export type IPalletIdentityRegistrarInfo = {
-  account: string;
-  fee: string;
-  fields: number[];
-  info: IRegistrarInfo;
-  judgements: object[][];
-  regIndex: number;
+	account: string;
+	fee: string;
+	fields: number[];
+	info: IRegistrarInfo;
+	judgements: object[][];
+	regIndex: number;
 };
 
 type IProps = {
-  registrarInfo: IPalletIdentityRegistrarInfo;
+	registrarInfo: IPalletIdentityRegistrarInfo;
 };
 
 export default function UserCard({ registrarInfo }: IProps) {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  return (
-    <Card>
-      <div className="block mb-3">
-        <div className="flex items-start gap-3">
-          {registrarInfo.info?.image?.Raw ? (
-            <img className="h-16 w-16 rounded-full" src={registrarInfo.info.image.Raw as string} alt="" />
-          ) : (
-            <Icon icon="person" size={IconSize.LARGE * 3} title="No image"></Icon>
-          )}
-          <div>
-            {registrarInfo.info?.display?.Raw ? (
-              <div className="text-lg">{registrarInfo.info?.display?.Raw as string}</div>
-            ) : (
-              <div className="text-lg">{t("user_card.lbl_no_display_name")}</div>
-            )}
-            <AccountName address={registrarInfo.account} />
-          </div>
-        </div>
-      </div>
+	return (
+		<Card>
+			<div className="block mb-3">
+				<div className="flex items-start gap-3">
+					{registrarInfo.info?.image?.Raw ? (
+						<img
+							className="h-16 w-16 rounded-full"
+							src={registrarInfo.info.image.Raw as string}
+							alt=""
+						/>
+					) : (
+						<Icon icon="person" size={IconSize.LARGE * 3} title="No image" />
+					)}
+					<div>
+						{registrarInfo.info?.display?.Raw ? (
+							<div className="text-lg">
+								{registrarInfo.info?.display?.Raw as string}
+							</div>
+						) : (
+							<div className="text-lg">
+								{t("user_card.lbl_no_display_name")}
+							</div>
+						)}
+						<AccountName address={registrarInfo.account} />
+					</div>
+				</div>
+			</div>
 
-      <div className="grid grid-cols-2 gap-3">
-        <HTMLTable>
-          <tbody>
-            <tr>
-              <td className="shadow-none">{t("user_card.lbl_email")}</td>
-              <td className="shadow-none">{registrarInfo.info?.email.Raw as string}</td>
-            </tr>
-            <tr>
-              <td>{t("user_card.lbl_website")}</td>
-              <td>{registrarInfo.info?.web.Raw as string}</td>
-            </tr>
-            <tr>
-              <td>{t("user_card.lbl_twitter")}</td>
-              <td>{registrarInfo.info?.twitter.Raw as string}</td>
-            </tr>
-            {registrarInfo.info.additional?.map((addItem, i) => {
-              return (
-                <tr key={i}>
-                  {addItem?.map((item, j) => {
-                    return <td key={item.Raw}>{item.Raw}</td>;
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </HTMLTable>
-        {registrarInfo.judgements && (
-          <div className="mb-2">
-            <Tag round large className={`${Classes.BREADCRUMB_CURRENT} bg-green-700 text-green-100`}>
-              {typeof registrarInfo.judgements[0][1] == "object"
-                ? Object.keys(registrarInfo.judgements[0][1]).map((key, index): any => {
-                    if (key in registrarInfo.judgements[0][1]) {
-                      return (
-                        <span key={index}>
-                          {key}: {(registrarInfo.judgements[0][1] as Record<string, string>)[key].toString()}
-                        </span>
-                      );
-                    }
-                    return <span key={index}></span>;
-                  })
-                : registrarInfo.judgements?.[0] && registrarInfo.judgements[0][1]}
-            </Tag>
-          </div>
-        )}
-      </div>
-    </Card>
-  );
+			<div className="grid grid-cols-2 gap-3">
+				<HTMLTable>
+					<tbody>
+						<tr>
+							<td className="shadow-none">{t("user_card.lbl_email")}</td>
+							<td className="shadow-none">
+								{registrarInfo.info?.email.Raw as string}
+							</td>
+						</tr>
+						<tr>
+							<td>{t("user_card.lbl_website")}</td>
+							<td>{registrarInfo.info?.web.Raw as string}</td>
+						</tr>
+						<tr>
+							<td>{t("user_card.lbl_twitter")}</td>
+							<td>{registrarInfo.info?.twitter.Raw as string}</td>
+						</tr>
+						{registrarInfo.info.additional?.map((addItem, i) => {
+							return (
+								<tr key={`row-${addItem[0]?.Raw}-${i}`}>
+									{addItem?.map((item, j) => {
+										return <td key={`${item.Raw}-${j}`}>{item.Raw}</td>;
+									})}
+								</tr>
+							);
+						})}
+					</tbody>
+				</HTMLTable>
+				{registrarInfo.judgements && (
+					<div className="mb-2">
+						<Tag
+							round
+							large
+							className={`${Classes.BREADCRUMB_CURRENT} bg-green-700 text-green-100`}
+						>
+							{typeof registrarInfo.judgements[0][1] === "object"
+								? Object.keys(registrarInfo.judgements[0][1]).map(
+										(key, index): JSX.Element => {
+											if (key in registrarInfo.judgements[0][1]) {
+												return (
+													<span key={key}>
+														{key}:{" "}
+														{(
+															registrarInfo.judgements[0][1] as Record<
+																string,
+																string
+															>
+														)[key].toString()}
+													</span>
+												);
+											}
+											return <span key={key} />;
+										},
+									)
+								: registrarInfo.judgements?.[0]?.[1]}
+						</Tag>
+					</div>
+				)}
+			</div>
+		</Card>
+	);
 }
