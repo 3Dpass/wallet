42a121e109485a406869c4bf243e6573a8d169a6
Mikhail Fedosov <tbs.micle@gmail.com>
2025-03-30 21:42:27 +0400

    feat(network): add circulating supply and total issuance APIs

---
+++ app/routes/network.totalIssuance.tsx
@@ -0,0 +1,31 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { BigInt as PolkaBigInt } from "@polkadot/x-bigint";
+import { RPC_CONFIG, RPC_TYPES } from "../api.config";
+import { defaultEndpoint } from "../atoms";
+
+async function getTotalIssuance() {
+  const provider = new WsProvider(defaultEndpoint, false);
+  await provider.connect();
+  const api = await ApiPromise.create({
+    provider,
+    rpc: RPC_CONFIG,
+    types: RPC_TYPES,
+  });
+
+  const totalIssuance = await api.query.balances.totalIssuance();
+  const totalIssuanceNumber = PolkaBigInt(
+    totalIssuance.toPrimitive() as number
+  );
+
+  return (totalIssuanceNumber / PolkaBigInt(10 ** 12)).toString();
+}
+
+export async function loader() {
+  const totalIssuance = await getTotalIssuance();
+  return new Response(totalIssuance, {
+    headers: {
+      "Cache-Control": "public, s-maxage=60",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
