330f233437b08d4fe50f260f0708da101df47e74
Mikhail Fedosov <tbs.micle@gmail.com>
2025-03-31 20:02:07 +0400

    refactor(common): remove unused ExplorerForward and routes

--- app/components/common/ExplorerForward.tsx
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://3dpscan.io";
-
-export const ExplorerUrl = {
-  extrinsic: (blockNumber: number, extrinsicIdx: number) =>
-    `${BASE_URL}/extrinsic/${blockNumber}-${extrinsicIdx}`,
-  account: ({ address }: { address?: string }) =>
-    `${BASE_URL}/account/${address}`,
-  block: ({ block }: { block?: string }) => `${BASE_URL}/block/${block}`,
-};
